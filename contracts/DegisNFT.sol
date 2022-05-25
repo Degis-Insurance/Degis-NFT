a0cd419282a4eeed4718f8a32e4269fab35f42e4
liyang-WSL <ylikp.ust@gmail.com>
2022-05-25 11:53:54 +0800

    add withdrawERC20&refund and modify comments

--- contracts/DegisNFT.sol
+++ contracts/DegisNFT.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
@@ -18,11 +19,10 @@ contract DegisNFT is ERC721, Ownable, Pausable {
     uint256 public mintedAmount;
 
     // wallet mapping that allows wallets to mint during airdrop and allowlist sale
-    mapping (address => bool) public allowlist;
-    mapping (address => bool) public airdroplist;
+    mapping(address => bool) public allowlist;
+    mapping(address => bool) public airdroplist;
     // amount minted on public sale
-    mapping (address => uint256) public mintedOnPublic;
-
+    mapping(address => uint256) public mintedOnPublic;
 
     uint256 public constant maxMintSupply = 499;
     uint256 public constant mintPrice = 1 ether;
@@ -33,6 +33,11 @@ contract DegisNFT is ERC721, Ownable, Pausable {
     string public baseURI;
 
     event StatusChange(Status oldStatus, Status newStatus);
+    event WithdrawERC20(
+        address indexed token,
+        uint256 amount,
+        address receiver
+    );
 
     error WrongStatus();
 
@@ -41,8 +46,8 @@ contract DegisNFT is ERC721, Ownable, Pausable {
     }
 
     /**
-     * @notice changes minting status
-     * @param  _newStatus new minting status
+     * @notice Change minting status
+     * @param  _newStatus New minting status
      */
     function setStatus(Status _newStatus) external onlyOwner {
         emit StatusChange(status, _newStatus);
@@ -50,8 +55,8 @@ contract DegisNFT is ERC721, Ownable, Pausable {
     }
 
     /**
-     * @notice Sets the base URI for the NFTs
-     * @param  _baseURI new base URI for the collection
+     * @notice Set the base URI for the NFTs
+     * @param  _baseURI New base URI for the collection
      */
     function setBaseURI(string calldata _baseURI) external onlyOwner {
         baseURI = _baseURI;
@@ -59,9 +64,9 @@ contract DegisNFT is ERC721, Ownable, Pausable {
 
     /**
      * @notice Owner minting
-     * @param  _quantity amount of NFTs to mint
+     * @param  _quantity Amount of NFTs to mint
      */
-    function ownerMint(uint _quantity) external onlyOwner {   
+    function ownerMint(uint256 _quantity) external onlyOwner {
         _mint(msg.sender, _quantity);
         mintedAmount += _quantity;
     }
@@ -69,7 +74,7 @@ contract DegisNFT is ERC721, Ownable, Pausable {
     /**
      * @notice Claimable NFTs
      */
-    function airdropClaim() external {   
+    function airdropClaim() external {
         if (status != Status.AirdropClaim) revert WrongStatus();
         require(airdroplist[msg.sender], "Only airdrop wallets");
         _mint(msg.sender, 1);
@@ -81,7 +86,7 @@ contract DegisNFT is ERC721, Ownable, Pausable {
      * @notice Allowlist minting
      * @param  _quantity amount of NFTs to mint
      */
-    function allowlistSale(uint _quantity) external payable {
+    function allowlistSale(uint256 _quantity) external payable {
         if (status != Status.AllowlistSale) revert WrongStatus();
         require(allowlist[msg.sender], "Only allowlist wallets");
         require(msg.value >= _quantity * allowPrice, "Not enough ether");
@@ -91,38 +96,58 @@ contract DegisNFT is ERC721, Ownable, Pausable {
         mintedAmount += _quantity;
     }
 
-
     /**
      * @notice  public sale mint. Allowed to mint several times as long as total per wallet is bellow maxPublicSale
      * @param  _quantity amount of NFTs to mint
      */
-    function publicSale(uint _quantity) external payable {
+    function publicSale(uint256 _quantity) external payable {
         if (status != Status.PublicSale) revert WrongStatus();
         require(tx.origin == msg.sender, "No proxy transactions");
-        require(msg.value >= _quantity * mintPrice, "Not enough ether");
-        require(mintedOnPublic[msg.sender] + _quantity <= maxPublicSale, "Max public sale reached");
-        require(mintedOnPublic[msg.sender] + _quantity + mintedAmount <= maxMintSupply, "Max mint supply reached");
+
+        uint256 amountToPay = _quantity * mintPrice;
+
+        require(msg.value >= amountToPay, "Not enough ether");
+        require(
+            mintedOnPublic[msg.sender] + _quantity <= maxPublicSale,
+            "Max public sale reached"
+        );
+        require(
+            mintedOnPublic[msg.sender] + _quantity + mintedAmount <=
+                maxMintSupply,
+            "Max mint supply reached"
+        );
         _mint(msg.sender, _quantity);
         mintedOnPublic[msg.sender] += _quantity;
         mintedAmount += _quantity;
+
+        // Refund
+        if (msg.value > amountToPay) {
+            payable(msg.sender).transfer(msg.value - amountToPay);
+        }
     }
 
     /**
      * @notice   adds wallets to airdrop list
      * @param  _addresses array of addresses to add to airdrop loop
      */
-    function addWalletsAirdrop(address[] calldata _addresses) external onlyOwner {
-        for (uint i=0; i< _addresses.length; i++) {
+    function addWalletsAirdrop(address[] calldata _addresses)
+        external
+        onlyOwner
+    {
+        for (uint256 i = 0; i < _addresses.length; i++) {
             airdroplist[_addresses[i]] = true;
         }
     }
 
-     /**
+    /**
      * @notice   adds wallets to allowlist
      * @param  _addresses array of addresses to add to airdrop loop
      */
-    function addWalletsAllowlist(address[] calldata _addresses) external onlyOwner {
-        for (uint i=0; i< _addresses.length; i++) {
+    function addWalletsAllowlist(address[] calldata _addresses)
+        external
+        onlyOwner
+    {
+        for (uint256 i = 0; i < _addresses.length; i++) {
             allowlist[_addresses[i]] = true;
         }
     }
@@ -133,5 +158,11 @@ contract DegisNFT is ERC721, Ownable, Pausable {
     function withdraw() external onlyOwner {
         payable(msg.sender).transfer(address(this).balance);
     }
+
     // implement another withdraw function for any other type of token?
+
+    function withdrawERC20(address _token, uint256 _amount) external onlyOwner {
+        IERC20(_token).transfer(msg.sender, _amount);
+        emit WithdrawERC20(_token, _amount, msg.sender);
+    }
 }
