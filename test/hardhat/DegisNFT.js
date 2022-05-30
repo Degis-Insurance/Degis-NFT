2bb223ddfcc9302079b866e17aede344d027eadc
liyang-WSL <ylikp.ust@gmail.com>
2022-05-30 10:52:22 +0800

    add test

--- test/hardhat/DegisNFT.js
+++ test/hardhat/DegisNFT.js
@@ -25,7 +25,7 @@ describe("Degis NFT Mint", function () {
     })
 
 
-    describe("Constants settings", function () {
+    describe("Deployments", function () {
         it("should have the correct prices", async function () {
             expect(await nft.PRICE_PUBLICSALE()).to.equal(parseUnits("200", 18));
             expect(await nft.PRICE_ALLOWLIST()).to.equal(parseUnits("100"), 18);
@@ -38,6 +38,45 @@ describe("Degis NFT Mint", function () {
         it("should have the correct amounts", async function () {
             expect(await nft.MAXAMOUNT_PUBLICSALE()).to.equal(5);
             expect(await nft.MAXAMOUNT_ALLOWLIST()).to.equal(3);
+        });
+
+        it("should have the correct init status", async function () {
+            expect(await nft.status()).to.equal(await nft.STATUS_INIT());
+        });
+    })
+
+    describe("Airdrop Claim & Allowlist Sale", function () {
+        let leaves_airdrop, tree_airdrop, root_airdrop;
+        let leaves_allowlist, tree_allowlist, root_allowlist;
+
+        let proof_airdrop, wrongProof_airdrop, proof_allowlist, wrongProof_allowlist;
+
+        beforeEach(async function () {
+            const airdrop_list = [user1, user2];
+            leaves_airdrop = airdrop_list.map(account => keccak256(account.address));
+            tree_airdrop = new MerkleTree(leaves_airdrop, keccak256, { sort: true });
+            root_airdrop = tree_airdrop.getHexRoot();
+
+            const allowlist = [user3, user4];
+            leaves_allowlist = allowlist.map(account => keccak256(account.address));
+            tree_allowlist = new MerkleTree(leaves_allowlist, keccak256, { sort: true });
+            root_allowlist = tree_allowlist.getHexRoot();
+
+            // get proofs
+            proof_airdrop = tree_airdrop.getHexProof(keccak256(airdrop_list[0].address))
+            wrongProof_airdrop = tree_airdrop.getHexProof(keccak256(allowlist[0].address))
+
+            proof_allowlist = tree_allowlist.getHexProof(keccak256(allowlist[0].address))
+            wrongProof_allowlist = tree_allowlist.getHexProof(keccak256(airdrop_list[0].address))
+        });
+
+        it("should not be able to claim before setting status", async function () {
+            await expect(nft.airdropClaim(proof_airdrop)).to.be.rejectedWith('Not in airdrop phase');
+            await expect(nft.allowlistSale(1, proof_allowlist)).to.be.rejectedWith("Not in allowlist sale phase")
+        });
+
+        it("should be able to claim airdrops", async function () {
+
         })
     })
 })
